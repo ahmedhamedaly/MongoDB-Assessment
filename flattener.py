3b1af40ffecbd1076eb90e96e171ff79167b7ad5
Ahmed <ahmedhamedaly19@gmail.com>
2020-12-23 17:50:38 +0000

    method working

--- flattener.py
+++ flattener.py
@@ -1,5 +1,18 @@
 import json
 
 
+def flatten(json, flat={}, prefix=''):
+    for key, value in json.items():
+        pf_k = (prefix + '.' + key) if prefix else key
+
+        if type(value) is dict:
+            flatten(value, flat, pf_k)
+        else:
+            flat[pf_k] = value
+    
+    return flat
+
+
 with open('./test_files/sample.json', 'r') as f:
     json_file = json.load(f)
+    print(flatten(json_file))
